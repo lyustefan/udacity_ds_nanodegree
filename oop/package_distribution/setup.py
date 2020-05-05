2d445fb3cfbb1d8775815fe5ed2721f6033af30c
Yang Lyu <smart.lvyang@gmail.com>
2020-05-05 16:06:33 -0500

    update 1.0

---
+++ oop/package_distribution/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_package_yl',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dist_package_yl'],
+      author = 'Yang Lyu',
+      author_email = 'smart.lvyang@gmail.com',
+      zip_safe=False)
