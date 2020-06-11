111e2f26b264da325beed71aaff352e147dcc55e
Yang Lyu <smart.lvyang@gmail.com>
2020-06-11 18:34:40 -0500

    added dsyl

--- object_oriented_programming/ds_yl/setup.py
+++ object_oriented_programming/ds_yl/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='ds',
+setup(name='ds_yl',
       version='0.1',
       description='Common Functions',
-      packages=['ds'],
+      packages=['ds_yl'],
       author = 'Yang Lyu',
       author_email = 'smart.lvyang@gmail.com',
       zip_safe=False)
